d367caaf443e1d27892477d8b32dd798fba1e682
SolovyovKirill <Solowyow.kirya@gmail.com>
2021-05-20 22:11:57 +0300

    Homework 2 kotlin сделаны исправления

--- app/src/main/java/com/example/myapplication/homework2/Car.kt
+++ app/src/main/java/com/example/myapplication/homework2/Car.kt
@@ -3,12 +3,9 @@ package com.example.myapplication.homework2
 class Car (var brand: String,
            var color: String = "black",
            var engineCapacity: Double,
-           var licensePlate: String? = null){
+           var licensePlate: String?){
 
-    fun printCar () {
-        return println("Марка $brand, " +
-                "цвет $color, " +
-                "объём двигателя $engineCapacity, " +
-                "номер автомобиля ${licensePlate ?: "Transit"}.")
+    fun printCar ():String {
+        return println("Марка $brand, цвет $color, объём двигателя $engineCapacity, номер автомобиля ${licensePlate ?: "Transit"}.").toString()
     }
 }
